3376bd95eb22890d154c42b68115b598ca8f3c95
Aslan Taheri <fromthenarnia@gmail.com>
2023-04-29 18:42:26 -0400

    add component folders and set up the header react component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,12 @@
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header>
+        <h1>Meal Order App</h1>
+        <button>Cart</button>
+      </header>
+      <dvi></dvi>
+    </Fragment>
+  );
+};
+export default Header;
