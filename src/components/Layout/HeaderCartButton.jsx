6bbe996f8e5fc9e1fa7f35dd5e5a4319464ab967
Aslan Taheri <fromthenarnia@gmail.com>
2023-05-24 21:07:17 -0400

    Add Cart animation via useEffect in HeaderCartButton component with proper setTimeout logic for animation reset.

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,17 +1,35 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsAnimated, setBtnIsAnimated] = useState(false);
+
   const cartCtx = useContext(CartContext);
-  const numberOfCartItems = cartCtx.items.reduce((sum, item) => {
+  const { items } = cartCtx;
+  const numberOfCartItems = items.reduce((sum, item) => {
     return sum + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${btnIsAnimated ? classes.bump : ""}`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+
+    setBtnIsAnimated(true);
+    const timer = setTimeout(() => {
+      setBtnIsAnimated(false);
+    }, 400);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
