ea61ab21f0b8fdb273e33e2771d2fadfc8db7132
Aslan Taheri <fromthenarnia@gmail.com>
2023-06-14 14:44:06 -0400

    Connect to Firebase database and update the code for fetch in AvailableItems component.

--- src/components/Groceries/AvailableItems.jsx
+++ src/components/Groceries/AvailableItems.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableItems.module.css";
 import GroceryItem from "./GroceryItem/GroceryItem";
 
 const AvailableItems = () => {
-  const items = DUMMY_ITEMS.map((item) => (
+  const [groceries, setGroceries] = useState([]);
+  useEffect(() => {
+    const fetchBasket = async () => {
+      const response = await fetch(
+        "https://avocadoor-9ad68-default-rtdb.firebaseio.com/vegetables.json"
+      );
+      const responseData = await response.json();
+      const loadedItems = [];
+      for (const key in responseData) {
+        loadedItems.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setGroceries(loadedItems);
+    };
+    fetchBasket();
+  }, []);
+
+  const items = groceries.map((item) => (
     <GroceryItem
       key={item.id}
       id={item.id}
